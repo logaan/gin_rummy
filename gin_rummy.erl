d846e37702a56d105d03f77fe41540f443b9cc76
Colin Campbell-McPherson <colin@logaan.net>
2010-02-13 16:24:46 +0800

    dropped spaces inside brackets

--- gin_rummy.erl
+++ gin_rummy.erl
@@ -4,49 +4,49 @@
 -compile([export_all]).
 
 setup_database() ->
-  mnesia:delete_schema( [node()] ),
-  mnesia:create_schema( [node()] ),
+  mnesia:delete_schema([node()]),
+  mnesia:create_schema([node()]),
   mnesia:start(),
-  mnesia:create_table( card, [{disc_copies, [node()]},
+  mnesia:create_table(card, [{disc_copies, [node()]},
                               {attributes, record_info(fields, card)}]),
-  mnesia:create_table( game, [{disc_copies, [node()]},
+  mnesia:create_table(game, [{disc_copies, [node()]},
                               {attributes, record_info(fields, game)}]).
 
 teardown_database() ->
   mnesia:stop(),
-  mnesia:delete_schema( [node()] ).
+  mnesia:delete_schema([node()]).
 
-insert_card( Card ) ->
-  Fun = fun() -> mnesia:write( Card ) end,
-  mnesia:transaction( Fun ).
+insert_card(Card) ->
+  Fun = fun() -> mnesia:write(Card) end,
+  mnesia:transaction(Fun).
 
 new_deck() ->
   Fun = fun() -> qlc:e(qlc:q([ C || C <- mnesia:table(card) ])) end,
-  {atomic, Deck} = mnesia:transaction( Fun ),
-  shuffle_deck( Deck ).
+  {atomic, Deck} = mnesia:transaction(Fun),
+  shuffle_deck(Deck).
 
 % This was the original implementation:
-% shuffle_deck( Deck ) ->
-%   lists:sort( fun( _, _ ) -> random:uniform(2) == 1 end, Deck ).
+% shuffle_deck(Deck) ->
+%   lists:sort(fun(_, _) -> random:uniform(2) == 1 end, Deck).
 % But I think calling random_draw/1 will give a better distribution
-shuffle_deck( Deck ) ->
-  shuffle_deck( [], Deck ).
-shuffle_deck( Deck, [] ) ->
+shuffle_deck(Deck) ->
+  shuffle_deck([], Deck).
+shuffle_deck(Deck, []) ->
   Deck;
-shuffle_deck( Deck, OldDeck ) ->
-  {RandomCard, NewOldDeck} = random_draw( OldDeck ),
+shuffle_deck(Deck, OldDeck) ->
+  {RandomCard, NewOldDeck} = random_draw(OldDeck),
   NewDeck = [RandomCard | Deck],
-  shuffle_deck( NewDeck, NewOldDeck ).
+  shuffle_deck(NewDeck, NewOldDeck).
 
-random_draw( Deck ) ->
+random_draw(Deck) ->
   Card = lists:nth(random:uniform(length(Deck)), Deck),
   NewDeck = lists:delete(Card, Deck),
   {Card, NewDeck}.
 
-draw( [Card | Deck ] ) ->
+draw([Card | Deck ]) ->
   {Card, Deck}.
-draw( NumberOfCards, Deck ) ->
-  lists:split( NumberOfCards, Deck ).
+draw(NumberOfCards, Deck) ->
+  lists:split(NumberOfCards, Deck).
 
 generate_playing_cards() ->
   Suites = ["Hearts", "Diamonds", "Spades", "Clubs"],
@@ -57,10 +57,10 @@ generate_playing_cards() ->
       properties = [{suite, S}, {value, V}]
     } || S <- Suites, V <- Values ].
 
-start_game( Player1Name, Player2Name ) ->
+start_game(Player1Name, Player2Name) ->
   Deck1 = new_deck(),
-  {Player1Hand, Deck2} = draw( 10, Deck1 ),
-  {Player2Hand, Deck3} = draw( 10, Deck2 ),
+  {Player1Hand, Deck2} = draw(10, Deck1),
+  {Player2Hand, Deck3} = draw(10, Deck2),
   Player1 = #player{ name = Player1Name, hand = Player1Hand },
   Player2 = #player{ name = Player2Name, hand = Player2Hand },
   #game{ player1 = Player1, player2 = Player2, deck = Deck3, discard = [] }.
--- test.erl
+++ test.erl
@@ -4,21 +4,21 @@
 -record(game, {player1, player2, deck, discard}).
 -record(player, {name, hand}).
 
-main( _ ) ->
+main(_) ->
   make:files([gin_rummy]),
   gin_rummy:setup_database(),
 
   io:format("=== All Cards ===~n"),
   lists:map(fun gin_rummy:insert_card/1, gin_rummy:generate_playing_cards()),
   Deck = gin_rummy:new_deck() ,
-  [ display_card( C ) || C <- Deck ],
+  [ display_card(C) || C <- Deck ],
 
   io:format("=== Starting Game ===~n"),
-  Game = gin_rummy:start_game( "Colin", "Royce" ),
+  Game = gin_rummy:start_game("Colin", "Royce"),
   io:format("~p cards in the deck~n", [length(Game#game.deck)]),
   io:format(io_lib:print(Game)),
 
   gin_rummy:teardown_database().
 
-display_card( {card, Name, _Properties} ) ->
+display_card({card, Name, _Properties}) ->
   io:format("~s~n", [Name]).
